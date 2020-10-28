dc4d949ac906c78925c5acc5cef796d2abb0bbce
FabienNiepceron <fabienniepceron72@gmail.com>
2020-10-28 15:33:33 +0100

    Modification du à l'exercice react08

--- src/App.js
+++ src/App.js
@@ -2,35 +2,35 @@ import React from "react";
 import SimpsonsQuotes from "./components/SimpsonsQuotes";
 import axios from "axios";
 
-const sampleSimpsons = {
-  quote: "They taste like...burning.",
-  character: "Ralph Wiggum",
-  image:
-    "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523",
-  characterDirection: "left",
-};
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      simpsons: sampleSimpsons,
+      simpsons: null,
     };
     this.getSimpsons = this.getSimpsons.bind(this);
   }
-  getSimpsons() {
+  componentDidMount() {
+    this.getSimpsons();
+  }
+
+  getSimpsons = () => {
     axios
       .get("https://thesimpsonsquoteapi.glitch.me/quotes")
       .then((reponse) => reponse.data)
       .then((data) => {
         this.setState({ simpsons: data[0] });
       });
-  }
+  };
 
   render() {
     return (
       <div className="App">
-        <SimpsonsQuotes simpsons={this.state.simpsons} />;
+        {this.state.simpsons ? (
+          <SimpsonsQuotes simpsons={this.state.simpsons} />
+        ) : (
+          <p>no data</p>
+        )}
         <button type="button" onClick={this.getSimpsons}>
           Get Simpsons
         </button>
